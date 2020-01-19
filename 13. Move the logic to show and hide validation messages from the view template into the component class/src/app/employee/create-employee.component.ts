a8cdc3ca0eab62f3c212c2f5da54e3d9893d9f2e
Your Name <you@example.com>
2020-01-19 23:41:09 +0530

    first commit

---
+++ 13. Move the logic to show and hide validation messages from the view template into the component class/src/app/employee/create-employee.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+  employeeForm: FormGroup;
+
+  formErrors = {
+    'fullName': '',
+    'email': '',
+    'skillname': '',
+    'experience': '',
+    'proficiency': ''
+  };
+
+  // This object contains all the validation messages for this form
+  validationMessages = {
+    'fullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.'
+    },
+    'skillname': {
+      'required': 'Skill Name is required.',
+    },
+    'experience': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  };
+
+  ngOnInit() {
+
+    this.employeeForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+      email: ['', Validators.required],
+      skills: this.fb.group({
+        skillname: ['', Validators.required],
+        experience: ['', Validators.required],
+        proficiency: ['', Validators.required]
+      }),
+    });
+
+    // Subscribe to FormGroup valueChanges observable
+    // this.employeeForm.valueChanges.subscribe(
+    //   value => {
+    //     console.log(value);
+    //     console.log(JSON.stringify(value));
+    //   }
+    // );
+  }
+
+  loadvalues(): void {
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
+  }
+
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
+    // loop through each key in the FormGroup
+    Object.keys(group.controls).forEach((key: string) => {
+      // Get a reference to the control using the FormGroup.get() method
+      const abstractControl = group.get(key);
+      // If the control is an instance of FormGroup i.e a nested FormGroup
+      // then recursively call this same method (logKeyValuePairs) passing it
+      // the FormGroup so we can get to the form controls in it
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+        // If the control is not a FormGroup then we know it's a FormControl
+
+
+      } else {
+        // console.log('Key = ' + key + ' && Value = ' + abstractControl.value);
+        // abstractControl.disable();
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+          // Get all the validation messages of the form control
+          // that has failed the validation
+          const messages = this.validationMessages[key];
+          // Find which validation has failed. For example required,
+          // minlength or maxlength. Store that error message in the
+          // formErrors object. The UI will bind to this object to
+          // display the validation errors
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.employeeForm.value);
+    console.log(this.employeeForm);
+
+
+  }
+
+
+}
